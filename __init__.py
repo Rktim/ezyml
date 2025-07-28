1fdd264e60ed230342420be520cc4b3b2deb9e4f
Raktim Kalita <155469713+Rktim@users.noreply.github.com>
2025-07-28 20:57:39 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# ezyml/__init__.py
+
+# This file makes the 'ezyml' directory a Python package.
+
+# Import the main class to make it directly accessible to users
+from .ezyml import EZTrainer
+
+__version__ = "0.1.0"
+__author__ = "Raktim Kalita"
