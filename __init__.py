1273d95c1c32eeea3e42c31b88ae432bbef680b9
Raktim Kalita <155469713+Rktim@users.noreply.github.com>
2025-07-30 11:13:50 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@
 # This file makes the 'ezyml' directory a Python package.
 
 # Import the main class to make it directly accessible to users
-from .ezyml import EZTrainer
+from .core import EZTrainer
 
 __version__ = "1"
 __author__ = "Raktim Kalita"
